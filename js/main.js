33b627ec1221df6b5fb7cd288f6bcf653317dad1
davehernaez <dave.hernaez@yahoo.com>
2016-09-28 15:56:56 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+smoothScroll.init({
+    speed: 800,
+    easing: 'easeInOutCubic',
+    updateURL: true,
+    offset: 100,
+});
+
+$(document).ready(function() {
+   $(".fancybox").fancybox();
+});
